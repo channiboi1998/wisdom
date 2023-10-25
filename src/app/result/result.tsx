15308daf6c1fa968bb50f31fd3d3017c5b22de4d
christian <christian.verzosa@jfcvendor.com>
2023-10-25 13:30:01 +0800

    feat(advice): dynamic advice message on result page

--- src/app/result/result.tsx
+++ src/app/result/result.tsx
@@ -1,19 +1,33 @@
 "use client";
 
+import { NeedEntrySkeleton } from "@/services/contentful";
 import { useMessageStore } from "@/zustand/message";
+import { useNeedStore } from "@/zustand/need";
 import { Screen, useScreenStore } from "@/zustand/screen";
+import { Entry } from "contentful";
 import Link from "next/link";
 import React from "react";
 
 const Result = () => {
+  const { selectedNeed } = useNeedStore();
   const { setSelectedScreen } = useScreenStore();
   const { randomMessage } = useMessageStore();
+
+  const adviceMessage = (
+    selectedNeed: Entry<NeedEntrySkeleton, undefined, string> | null
+  ): string | null => {
+    if (selectedNeed) {
+      if (selectedNeed.fields.label === "Reflection") {
+        return "Feel free to reflect in a way that resonates with you—whether through conversation, writing, drawing, walking, or simply sitting in silence.";
+      } else if (selectedNeed.fields.label === "Affirmation") {
+        return "If you are open to it, we recommend reading the following out loud. Stay here however long you need.";
+      }
+    }
+    return null;
+  };
   return (
     <div className="h-screen flex flex-col items-center justify-center">
-      <p>
-        If you are open to it, we recommend reading the following out loud. Stay
-        here however long you need.
-      </p>
+      <p>{adviceMessage(selectedNeed)}</p>
       <p>
         {randomMessage
           ? randomMessage.fields.message
