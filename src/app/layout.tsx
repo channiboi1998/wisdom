064c72895c8a1cc08f5f067e9cb5c2acbb386b22
christian <christian.verzosa@jfcvendor.com>
2023-11-01 17:42:29 +0800

    feat: disclaimer modal with responsive design

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
 import Disclaimer from "@/components/modals/disclaimer";
 import "./globals.css";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Jost } from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
+const jost = Jost({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${inter.className} relative min-h-screen flex flex-col items-center justify-center`}
+        className={`${jost.className} relative min-h-screen flex flex-col items-center justify-center`}
       >
         <Disclaimer />
         {children}
--- src/components/modals/disclaimer/disclaimer.tsx
+++ src/components/modals/disclaimer/disclaimer.tsx
@@ -15,32 +15,31 @@ const Disclaimer = () => {
 
   return (
     <Dialog
-      className="min-h-screen absolute top-0 left-0 w-full flex flex-row items-center justify-center z-40 bg-bootstrap-leather"
+      className="z-100 absolute left-0 top-0 flex h-screen w-full flex-col items-center justify-center bg-bootstrap-leather"
       open={disclaimer.isOpen}
       onClose={closeModal}
     >
-      <Dialog.Panel className="max-w-xl bg-white p-5">
-        <div className="flex flex-row justify-between items-center">
-          <p>Hi There!</p>
-          <button onClick={closeModal}>
-            <XMarkIcon className="h-8 w-8" />
+      <Dialog.Panel className="relative flex h-full w-10/12 max-w-7xl flex-col items-center justify-center lg:text-center">
+        <div className="relative w-full py-8 md:py-14">
+          <button className="absolute focus:outline-none right-0 top-0" onClick={closeModal}>
+            <XMarkIcon className="h-8 text-catalina-tile lg:h-10" />
           </button>
+          <p className="py-2 text-lg tracking-wide text-catalina-tile lg:py-4 lg:text-2xl lg:font-semibold xl:text-4xl">
+            Before you begin, we wish to emphasize that this service is not a
+            substitute for professional mental health assistance. If you are
+            facing a crisis, we encourage you to call or text a mental health
+            support line available in your area. If you ever find that your
+            emotions become too uncomfortable during your experience here, don’t
+            hesitate to step away and prioritize your self-care.
+          </p>
+          <p className="py-2 text-lg tracking-wide text-catalina-tile lg:py-4 lg:text-2xl lg:font-semibold xl:text-4xl">
+            We recognize that mental health support varies from person to
+            person, and our platform may not be the right fit for everyone.
+            Please feel free to take what resonates with you and leave what does
+            not. Our ultimate hope is that you find the support you need and
+            rightfully deserve.
+          </p>
         </div>
-        <p>
-          Before you begin, we wish to emphasise that this service is not a
-          substitute for professional mental health assistance. If you are
-          facing a crisis, we encourage you to call or text a mental health
-          support line available in your area. Throughout your experience here,
-          if you ever find that your emotions become too uncomfortable, don’t
-          hesitate to step away and prioritise your self-care.
-        </p>
-        <p>
-          We recognise that mental health support varies from person to person,
-          and our platform may not be the perfect fit for everyone. Please feel
-          free to take what resonates with you and leave what does not. Our
-          ultimate hope is that you find the support you need and rightfully
-          deserve.
-        </p>
       </Dialog.Panel>
     </Dialog>
   );
