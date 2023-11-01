c2f9722418ccdae2bad5e38a3fc67d2e881814dd
christian <christian.verzosa@jfcvendor.com>
2023-11-01 10:25:54 +0800

    fix: fix environment variable name

--- src/services/contentful.ts
+++ src/services/contentful.ts
@@ -26,7 +26,7 @@ export type MessageEntrySkeleton = {
 const useContentful = () => {
   ``;
   const client = contentful.createClient({
-    space: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE || "",
+    space: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID || "",
     accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN || "",
     host: process.env.NEXT_PUBLIC_CONTENTFUL_HOST || "",
   });
